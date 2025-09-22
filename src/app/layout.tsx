7a16c3bd04efb91d94ff9b4098f1d1e1517d1ff7
Alan <costealan@gmail.com>
2025-09-22 13:19:54 -0600

    refactor: move Header and nav to the layout
    these elements are static throught the page, what is going to change is whats under Main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
+import Link from "next/link";
 
 const railway = Raleway({
   subsets: ["latin"],
@@ -18,7 +20,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${railway} antialiased`}>{children}</body>
+      <body className={`${railway} antialiased`}>
+        <div className="flex justify-center">
+          <div className="flex max-w-7xl pt-40 pr-25 pb-23 pl-45 ">
+            <Header />
+            <div>
+              <nav className="ml-30 flex gap-5 pb-5">
+                <Link className="text-base-blue" href={"/about"}>
+                  About
+                </Link>
+                <Link className="text-base-blue" href={"/projects"}>
+                  Projects
+                </Link>
+              </nav>
+              <main className="max-w-125 ml-30">{children}</main>
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
