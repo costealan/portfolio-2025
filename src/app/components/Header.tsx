cf2076a3856354f9f41f789d7e8f684a86dae44e
Alan <costealan@gmail.com>
2025-09-22 12:07:09 -0600

    feat: add styles to the Header and positioned it for desktop version

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,18 +2,13 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex-1">
-      <div>
-        <h1>Alan Coste</h1>
-        <h3>Fullstack developer</h3>
-        <p>
-          I build <em>accesible</em> fullstack applications for the web
-        </p>
-      </div>
-      <nav>
-        <p>About</p>
-        <p>Projects</p>
-      </nav>
+    <header className="flex-1 font-bold">
+      <h1 className="text-5xl text-dark-blue">Alan Coste</h1>
+      <h3 className="text-2xl text-base-blue">Fullstack developer</h3>
+      <p className="pt-5 text-base-blue">
+        I build <em className="text-highlight-blue">accesible</em> fullstack
+        applications for the web
+      </p>
     </header>
   );
 }
--- src/app/food-content/page.tsx
+++ src/app/food-content/page.tsx
@@ -1,17 +1,51 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <section>
+    <main>
+      <Link href={"/"}>Home</Link>
       <div className="flex justify-center">
         <h1>Cooking and photography porftolio</h1>
       </div>
-      <Image
-        src="https://res.cloudinary.com/di6vnxokz/image/upload/v1717617435/portafolio-2024/fotografia%20de%20comieda/cdytmpdhwrttsm1d7xdr.jpg"
-        alt="Chocolate cake"
-        width={500}
-        height={300}
-      />
-    </section>
+      <section className="flex flex-wrap gap-10 justify-center">
+        <Image
+          src="https://res.cloudinary.com/di6vnxokz/image/upload/q_auto/portafolio-2024/fotografia%20de%20comieda/cdytmpdhwrttsm1d7xdr.jpg"
+          alt="Chocolate cake"
+          width={500}
+          height={300}
+        />
+        <Image
+          src="https://res.cloudinary.com/di6vnxokz/image/upload/q_auto/portafolio-2024/fotografia%20de%20comieda/cdytmpdhwrttsm1d7xdr.jpg"
+          alt="Chocolate cake"
+          width={500}
+          height={300}
+        />
+        <Image
+          src="https://res.cloudinary.com/di6vnxokz/image/upload/q_auto/portafolio-2024/fotografia%20de%20comieda/cdytmpdhwrttsm1d7xdr.jpg"
+          alt="Chocolate cake"
+          width={500}
+          height={300}
+        />
+        <Image
+          src="https://res.cloudinary.com/di6vnxokz/image/upload/portafolio-2024/fotografia%20de%20comieda/lykwe7eyxuufasivi23q.jpg"
+          alt="Burger, fries and craft beer"
+          width={500}
+          height={300}
+        />
+        <Image
+          src="https://res.cloudinary.com/di6vnxokz/image/upload/portafolio-2024/fotografia%20de%20comieda/zx8x7qc5egbmoq5r4wyv.jpg"
+          alt="Smoked Short ribs"
+          width={500}
+          height={300}
+        />
+        <Image
+          src="https://res.cloudinary.com/di6vnxokz/image/upload/v1717778460/portafolio-2024/fotografia%20de%20comieda/syjtkkniji9liyeftyws.jpg"
+          alt="Saque Mate"
+          width={500}
+          height={300}
+        />
+      </section>
+    </main>
   );
 }
